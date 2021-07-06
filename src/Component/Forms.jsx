13885bf5b56d45a83954ba62369f1276b3272486
prince antwi <antwi2k@gmail.com>
2021-07-06 14:36:13 +0800

    Added Form.jxs

---
+++ src/Component/Forms.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import "./User.css";
+class Forms extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullname: "",
+      email: "",
+      contact: "",
+      gen: "",
+    };
+  }
+  handleChange = (e) => {
+    {
+      const name = e.target.name;
+      const value = e.target.value;
+
+      this.setState({ [name]: value });
+    }
+  };
+  handelsubmite = (e) => {
+    e.preventDefault();
+    this.props.adduser(this.state);
+    this.setState({
+      username: "",
+      email: "",
+      password: "",
+      contact: "",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handelsubmite} className="user">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="fullname"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <br />
+          <label htmlFor="password">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <br />
+          <label htmlFor="Gen">Gen</label>
+          <input
+            type="number"
+            name="gen"
+            value={this.state.gen}
+            onChange={this.handleChange}
+          />
+
+          <br />
+          <label htmlFor="contact">Contact</label>
+          <input
+            type="number"
+            name="contact"
+            value={this.state.contact}
+            onChange={this.handleChange}
+          />
+          <br />
+          <button type="submit">Submit </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Forms;
