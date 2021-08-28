d246bf517b926fa729badd25688611222e584b75
princeantwi2k <antwi2k@gmail.com>
2021-08-28 11:31:56 +0800

    added Actions

--- src/Action/AddAction.js
+++ src/Action/AddAction.js
@@ -1,8 +1,23 @@
 export const ADD_USER = "ADD_USER"
+export const DELETE_USER ="DELETE_USER"
+export const EDIT_USER ="EDIT_USER"
 
 export const AddAction=(user)=>{
   return{ 
      type: ADD_USER,
     payload:user
 }
+}
+
+export const deleteUser=(user_id)=>{
+  return{ 
+     type: DELETE_USER,
+    payload:user_id
+} 
+}
+export const editUser=(updateUser)=>{
+  return{ 
+     type: EDIT_USER,
+    payload:updateUser
+} 
 }
