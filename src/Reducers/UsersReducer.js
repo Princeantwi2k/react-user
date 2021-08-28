a770ce445d755e86407b3697d303e85d93fa6c75
princeantwi2k <antwi2k@gmail.com>
2021-08-28 11:33:48 +0800

    added usersReducer

--- src/Reducers/UsersReducer.js
+++ src/Reducers/UsersReducer.js
@@ -1,26 +1,8 @@
 
-import { ADD_USER } from "../Action/AddAction"
+import { ADD_USER, DELETE_USER ,EDIT_USER} from "../Action/AddAction"
 
 const initialState = {users: [
-    {
-      fullname: "prince",
-      email: "antwi2k@gmail.com",
-      gen: 23,
-      contact: "09873944330",
-    },
-
-    {
-      fullname: "antwi",
-      email: "prince@email.com",
-      gen: 4,
-      contact: "02311398390",
-    },
-    {
-      fullname: "eunege",
-      email: "eunege@email.com",
-      gen: 7,
-      contact: "0987398390",
-    },
+  
   ],}
 
   let UsersReducers =  (state = initialState , action) => { 
@@ -28,9 +10,16 @@ const initialState = {users: [
           case ADD_USER :
               return {...state,users:[...state.users,action.payload]};
              
-      
+              case DELETE_USER:
+                let undeleted = state.users.filter((user) => user.id !== action.payload)
+               return {...state,users:undeleted}
+
+        case EDIT_USER:
+          const newUser = state.users.map((users)=>users.id === action.payload.id ? action.payload : users)
+          return {...state,users : newUser}
           default:
               return state
       }
   }
-  export default UsersReducers
+ 
+  export default  UsersReducers
