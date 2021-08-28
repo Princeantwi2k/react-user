e68d027b1fa9a69e70cd5d623bac72e5d307454e
princeantwi2k <antwi2k@gmail.com>
2021-08-28 11:32:55 +0800

    added User

--- src/Component/User.jsx
+++ src/Component/User.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 import { connect } from "react-redux";
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import EditUser from "./EditUser";
+import { deleteUser } from "../Action/AddAction";
 
-const User = ({ users }) => {
+const User = ({ users, deleteUser, editUser }) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    deleteUser(users.id);
+  };
   return (
-    <div>
-      {users.map((user, index) => {
-        return (
-          <div key={index}>
-            <h3>Fullname :{user.fullname}</h3>
-            <p>email :{user.email}</p>
-            <p>gen :{user.gen}</p>
-            <p>contact : {user.contact}</p>
-            <hr />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Contact</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <EditUser
+            users={users}
+            editUser={editUser}
+            closeModal={handleClose}
+          />
+        </Modal.Body>
+      </Modal>
+      <Card style={{ width: "18rem" }}>
+        <h2>Name : {users.fullname}</h2>
+        <p>Email : {users.email}</p>
+        <p>Gen : {users.gen}</p>
+        <p>Contact : {users.contact}</p>
+        <Button variant="outline-primary" size="sm" onClick={handleShow}>
+          Eidth
+        </Button>{" "}
+        <Button variant="outline-dark" size="sm" onClick={handleDelete}>
+          Delete
+        </Button>
+      </Card>
+    </>
   );
 };
-const mapStateToProps = (state) => {
-  return { users: state.users };
+const mapDispatchToProps = {
+  deleteUser: deleteUser,
 };
 
-export default connect(mapStateToProps)(User);
+export default connect(null, mapDispatchToProps)(User);
