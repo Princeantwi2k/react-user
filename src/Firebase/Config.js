5f0fa65be652ebb1df220181edc9db33077884ad
princeantwi2k <antwi2k@gmail.com>
2021-09-08 13:13:36 +0800

    added Config

---
+++ src/Firebase/Config.js
@@ -0,0 +1,20 @@
+
+import firebase from "firebase";
+
+ 
+
+
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyDnXZ_fLT6wcyWqoTTxP7ocxB69p6XnPeE",
+    authDomain: "my-user-b6391.firebaseapp.com",
+    projectId: "my-user-b6391",
+    storageBucket: "my-user-b6391.appspot.com",
+    messagingSenderId: "700852575983",
+    appId: "1:700852575983:web:bd9f752ea2cb604a11051a"
+  };
+  
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+   export default firebase;
