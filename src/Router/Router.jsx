558fc2fa17951f60d652f5f9d023c5c553596dd5
princeantwi2k <antwi2k@gmail.com>
2021-10-06 15:28:24 +0800

    added Router.jsx

---
+++ src/Router/Router.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+import Appj from "../Component/Appj";
+import Registration from "../Pages/Registration";
+import Login from "../Pages/Login";
+import ProtectedRoute from "../Component/ProtectedRoute/ProtectedRoute";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <ProtectedRoute exact path="/" component={Appj} />
+      <Route path="/home" component={Appj} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Registration} />
+    </BrowserRouter>
+  );
+};
+
+export default Router;
