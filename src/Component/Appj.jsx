3403093c88a74fed1a35ebaa177dc0024b495361
princeantwi2k <antwi2k@gmail.com>
2021-10-06 15:25:03 +0800

    added Appj.jsx

--- src/Component/Appj.jsx
+++ src/Component/Appj.jsx
@@ -1,47 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import Forms from "./Forms";
-
 import User from "./Userlist";
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [
-        {
-          fullname: "prince",
-          email: "antwi2k@gmail.com",
-          gen: 23,
-          contact: "09873944330",
-        },
-
-        {
-          fullname: "antwi",
-          email: "prince@email.com",
-          gen: 4,
-          contact: "02311398390",
-        },
-        {
-          fullname: "eunege",
-          email: "eunege@email.com",
-          gen: 7,
-          contact: "0987398390",
-        },
-      ],
-    };
-  }
-  handleAddUser = (user) => {
-    this.setState({ users: [...this.state.users], user });
-  };
-
-  render() {
-    return (
-      <>
-        <User users={this.state.users} />
-        <Forms adduser={this.handleAddUser} />
-      </>
-    );
-  }
-}
-
-export default App;
+import { useHistory } from "react-router";
+import { connect, useDispatch } from "react-redux";
+import { logout } from "./AuthAction/Auth";
+const Appj = (props) => {
+  const dispatch = useDispatch();
+  const { replace } = useHistory();
+  return (
+    <div>
+      <button onClick={() => dispatch(logout(replace))}>Logout</button>
+      <User />
+      <Forms />
+    </div>
+  );
+};
+const mapStateToProps = (state) => ({
+  isAuth: state.auth.isAuth,
+});
+export default connect(mapStateToProps)(Appj);
