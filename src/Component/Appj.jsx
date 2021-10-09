1b8b2afe9bbe4f2b5bb2b13e058d9a8b9a981afa
princeantwi2k <antwi2k@gmail.com>
2021-10-09 12:23:13 +0800

    added Appj

--- src/Component/Appj.jsx
+++ src/Component/Appj.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Forms from "./Forms";
+import { Container, Row, Col } from "react-bootstrap";
 import User from "./Userlist";
-import { useHistory } from "react-router";
-import { connect, useDispatch } from "react-redux";
-import { logout } from "./AuthAction/Auth";
+
 const Appj = (props) => {
-  const dispatch = useDispatch();
-  const { replace } = useHistory();
   return (
-    <div>
-      <button onClick={() => dispatch(logout(replace))}>Logout</button>
-      <User />
-      <Forms />
-    </div>
+    <Container>
+      <Row>
+        <Col xs={6} md={4}>
+          <User />
+        </Col>
+        <Col xs={6} md={2} className="box">
+          <Forms />
+        </Col>
+      </Row>
+    </Container>
   );
 };
-const mapStateToProps = (state) => ({
-  isAuth: state.auth.isAuth,
-});
-export default connect(mapStateToProps)(Appj);
+
+export default Appj;
