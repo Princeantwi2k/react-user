74dd2edd25881fa6a8a67db8ad7b927aa09bf731
princeantwi2k <antwi2k@gmail.com>
2021-10-09 12:24:02 +0800

    added ProtectedRoute

--- src/Component/ProtectedRoute/ProtectedRoute.jsx
+++ src/Component/ProtectedRoute/ProtectedRoute.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Route, Redirect } from "react-router";
+import { Route, Redirect } from "react-router-dom";
+
 const Protectedroute = ({ component: Component, auth, ...rest }) => {
+  if (!auth.isLoaded) return null;
+  if (auth.uid) {
+    return (
+      <Route
+        {...rest}
+        render={(props) => {
+          return <Component {...props} />;
+        }}
+      />
+    );
+  }
   return (
     <Route
       {...rest}
       render={(props) => {
-        if (auth.isloaded && !auth.isEmpty) {
-          return <Component />;
-        } else if (!auth.isLoaded) return <h1>Loading...</h1>;
-        else return <Redirect to="/login" />;
+        return <Redirect to={{ pathname: "/login" }} />;
       }}
     />
   );
 };
-const mapStateToProps = (state) => ({ auth: state.firebase.auth });
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+  };
+};
 
 export default connect(mapStateToProps)(Protectedroute);
